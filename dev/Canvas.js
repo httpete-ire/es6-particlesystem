a1f5bd795e9749f51ac32ca8c65668c3004db9d3
Pete Redmond <redmondp@gmail.com>
2015-03-24 22:58:23 +0000

    Canvas class

---
+++ dev/Canvas.js
@@ -0,0 +1,14 @@
+class Canvas {
+
+    constructor(elem) {
+        this.canvas = getElem(elem);
+        this.context = this.canvas.getContext('2d');
+        this.canvasWidth = this.canvas.width;
+        this.canvasHeight = this.canvas.height;
+        this.tick = 0;
+    };
+
+    clear() {
+        this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    }
+}
